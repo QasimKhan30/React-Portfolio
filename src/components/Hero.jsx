474f5b2d217932ebb2dee9b9f558b7134fe1f88c
Qasim Khan <qasimkhan0515@gmail.com>
2024-12-25 11:32:03 +0500

    added images for experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                         animate='visible'
                         className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Qasim Khan
                      </motion.h1>
-                    <motion.span variants={container(0.5)} initial='hidden' animate='visible' className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">Software Engineer</motion.span>
+                    <motion.span variants={container(0.5)} initial='hidden' animate='visible' className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Software Engineer</motion.span>
                     <motion.p variants={container(1)} initial='hidden' animate='visible' className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
                 </div>
             </div>
@@ -31,7 +31,8 @@ const Hero = () => {
 
             <div className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex justify-center">
-                    <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1, delay:1.2}} src={ProfilePic} alt="ProfilePicture" className="rounded-full h-80 w-80" />
+                <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1, delay: 1.2}} src={ProfilePic} alt="ProfilePicture" className="rounded-full" style={{height: '20rem', width: '20rem', objectFit: 'cover', objectPosition: 'center top'}} />
+
                 </div>
             </div>
 
